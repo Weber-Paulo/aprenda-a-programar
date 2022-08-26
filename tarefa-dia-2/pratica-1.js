0f496d7c822efd369ac6f4c63be34168f65e4cae
Weber-Paulo <paulomorandini@gmail.com>
2022-08-25 23:10:17 -0300

    subindo quinto código

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email = "paulomorandini@gmail.com"
 let profissao = "Gerente"
 let idade = "38"
 let lugarConhecer = "Suiça"
+console.log(nome) 
+console.log(email) 
+console.log(profissao)
+console.log(idade)
+console.log(lugarConhecer)
