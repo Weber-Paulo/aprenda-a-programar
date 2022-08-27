e22ef1285d0e0df600d8081a84160e85e5e54062
Weber-Paulo <paulomorandini@gmail.com>
2022-08-26 22:29:22 -0300

    exercício-dia-3

---
+++ Dia-3/ex1.js
@@ -0,0 +1,12 @@
+/* Site de conteúdo adulto. Imagina que estamos trabalhando para um site de conteúdo adulto,
+quando carregamos a página n´s verificamos a idade do usuário logado e se essa idade for menor
+de 18 anos nós mostraremos a mensagem: Conteúdo proibido para menores.*/
+
+let nome = "Paulinho"
+let idade = 10
+
+if  (idade >= 18)   {
+    console.log(nome + " pode entrar na página.")
+} else {
+    console.log(nome + " não insista meu amigo. Acesso proibido")
+}
