d86d4433b713710eca94111c0d04eebf707aa3c3
Weber-Paulo <paulomorandini@gmail.com>
2022-08-27 12:57:51 -0300

    exercício-4

---
+++ Dia-3/ex4.js
@@ -0,0 +1,11 @@
+/* SESSÃO 1: DOMINANDO LOOPS E CONTADORES
+
+Exercício 1: Exibindo números de 1 a 10
+*/
+
+let contador = 1
+
+while (contador <= 10) {
+    console.log(contador)
+    contador = contador + 1
+}
