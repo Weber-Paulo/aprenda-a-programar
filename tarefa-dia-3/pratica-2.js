294aabff6acda38ad74d8e5397e4a333b122038d
Weber-Paulo <paulomorandini@gmail.com>
2022-08-28 19:54:56 -0300

    pratica-2.js

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,12 @@
+/* TABUADA DE 9 */
+
+let multiplicador = 9
+let n = 10
+let i = 1
+
+while (i <= n) {
+    let resultado = multiplicador * i
+    let textoDeExibicao = `${multiplicador} X ${i} = ${resultado}`
+    console.log(textoDeExibicao)
+    i++
+}
