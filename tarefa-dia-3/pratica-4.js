62fa0aad677327248a6dfe9f0fa3d060c43fd832
Weber-Paulo <paulomorandini@gmail.com>
2022-08-28 20:48:16 -0300

    pratica-4

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,18 @@
+/* VERIFICAR SER O NUMERO É PRIMO */
+let num = 17
+let primo = `O número ${num} é primo`
+
+if (num == 1) {
+    primo = `O número ${num} não é primo`
+}   else if (num == 2) {
+    primo = `O número ${num} é primo`
+} else {
+    let numero = true
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            let numero = false
+            primo = `O número ${num} não é primo`
+        }
+    }
+}
+console.log (primo)
